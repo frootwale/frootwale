028e683031b65f36b1d8e71a812dfbfbe340cf06
frootwale <you@example.com>
2014-06-05 12:46:02 +0530

    Create basic application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frootwale::Application.config.secret_token = '46176231ebae40dad9746a610a21c471303c82cdddf1f131fca772a6d3c0071745fd1bb93c415deeb3a0d224d5a0bd2ab08155c09d1268d6a038dc59fe3e41f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Frootwale::Application.config.session_store :cookie_store, key: '_frootwale_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Frootwale::Application.config.session_store :active_record_store
