164f9c5ea603ec15c9f2d6833ae81efef7c483a4
frootwale <you@example.com>
2014-06-12 14:00:15 +0530

    Added background functionality for signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,24 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :mobile_no, :email
+  attr_accessible :first_name, :last_name, :mobile_no, :email, :password, :remember_me
+  attr_accessor :password
+
   has_many :addresses
   has_many :orders
+
+  before_save :create_hashed_password
+  after_save :erase_password
+
+  private
+
+  def create_hashed_password
+  	
+  	unless password.blank?
+  		self.salt = AdminUser.create_salt(self.first_name) if salt.blank?
+  		self.hashed_password = AdminUser.create_hashed_password(password, salt)
+  	end
+  end
+
+  def erase_password
+  	self.password = nil
+  end
 end
