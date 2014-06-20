b722962ee97ab892a1e89d9ab9992f80a9d0464d
frootwale <you@example.com>
2014-06-20 20:11:16 +0530

    Added method in user model to parse strings for cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ActiveRecord::Base
   before_save :create_hashed_password
   after_save :erase_password
 
+  def self.parse_cart_details(str="")
+    str1 = str.split(',')
+    final_string = Array.new
+
+    str1.each do |x|
+      str2 = x.split(/[*,=]/)
+      final_string << str2
+    end
+
+    return final_string
+  end
+
   private
 
   def create_hashed_password
